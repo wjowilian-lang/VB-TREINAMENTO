5cea7c7a93d4d1d1010956e5581713207ea328b1
wjowilian-lang <wjowilian@gmail.com>
2026-06-04 11:26:30 -0300

    Update gpt.js

--- api/gpt.js
+++ api/gpt.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.OPENAI_KEY}`,
+        "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
       },
       body: JSON.stringify(req.body),
     });
